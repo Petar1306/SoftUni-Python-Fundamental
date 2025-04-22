711a72c7644d9f5ccfe7a4bad5cddd1365a76e66
Petar1306 <Petar1306@students.softuni.bg>
2025-04-22 21:00:15 +0100

    Create 3_word_reverse.py

---
+++ Basic Syntax, Conditional Statements, and Loops/3_word_reverse.py
@@ -0,0 +1,12 @@
+# word = input()
+# reverse_word = ""
+# for i in range(len(word)-1, -1, -1):
+#     reverse_word += word[i]
+# print(reverse_word)
+
+# word = input()
+# for i in range(len(word)-1, -1, -1):
+#     print(word[i], end='')
+
+word = input()
+print(word[::-1])
