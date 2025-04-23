7fff4e2ed9dcfba58ae86ea0a75a9b1b45ead8e2
Petar1306 <Petar1306@students.softuni.bg>
2025-04-23 18:02:12 +0100

    Create 8_how_much_coffee_do_you_need?.py

---
+++ Basic Syntax, Conditional Statements, and Loops/Exercise_1/8_how_much_coffee_do_you_need?.py
@@ -0,0 +1,15 @@
+command = input()
+coffe_counter = 0
+while command != "END":
+    if (command.lower() == "coding" or command.lower() == "cat" or command.lower() == "dog" or
+            command.lower() == "movie"):
+        if command.islower():
+            coffe_counter += 1
+        else:
+            coffe_counter += 2
+    if coffe_counter > 5:
+        print("You need extra sleep")
+        break
+    command = input()
+else:
+    print(coffe_counter)
