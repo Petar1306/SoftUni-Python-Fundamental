f97dadd1d80c4913e2e8ef06de401acfffd10111
Petar1306 <Petar1306@students.softuni.bg>
2025-04-23 18:04:56 +0100

    Create 9_sorting_hat.py

---
+++ Basic Syntax, Conditional Statements, and Loops/Exercise_1/9_sorting_hat.py
@@ -0,0 +1,17 @@
+command = input()
+while True:
+    if command == "Welcome!":
+        print("Welcome to Hogwarts.")
+        break
+    elif command == "Voldemort":
+        print("You must not speak of that name!")
+        break
+    if len(command) < 5:
+        print(f"{command} goes to Gryffindor.")
+    elif len(command) == 5:
+        print(f"{command} goes to Slytherin.")
+    elif len(command) == 6:
+        print(f"{command} goes to Ravenclaw.")
+    elif len(command) > 6:
+        print(f"{command} goes to Hufflepuff.")
+    command = input()
