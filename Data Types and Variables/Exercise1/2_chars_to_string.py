26528c9da360620f22811c2f2c219e791e2d0bf4
Petar1306 <Petar1306@students.softuni.bg>
2025-04-25 19:24:53 +0100

    Create 2_chars_to_string.py

---
+++ Data Types and Variables/Exercise1/2_chars_to_string.py
@@ -0,0 +1,6 @@
+character_1 = input()
+character_2 = input()
+character_3 = input()
+# concat_string = f"{character_1}{character_2}{character_3}"
+concat_string = character_1 + character_2 + character_3
+print(concat_string)
