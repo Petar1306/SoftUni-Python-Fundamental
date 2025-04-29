d68e67d435b2d8bf853534b5abc579fdb8bf57b1
Petar1306 <Petar1306@students.softuni.bg>
2025-04-29 19:42:52 +0100

    Create 9_snowballs.py

---
+++ Data Types and Variables/Exercise1/9_snowballs.py
@@ -0,0 +1,17 @@
+snowballs = int(input())
+value = 0
+highest_value = 0
+highest_weight = 0
+highest_time = 0
+highest_quality = 0
+for _ in range(snowballs):
+    weight = int(input())
+    time = int(input())
+    quality = int(input())
+    value = (weight / time) ** quality
+    if value > highest_value:
+        highest_value = value
+        highest_weight = weight
+        highest_time = time
+        highest_quality = quality
+print(f"{highest_weight} : {highest_time} = {int(highest_value)} ({highest_quality})")
